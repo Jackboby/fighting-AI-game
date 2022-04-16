dd42aea8b7f23ec439288d64e9d846f9bb610cb7
Jackboby <101992698+Jackboby@users.noreply.github.com>
2022-04-16 14:43:55 -0600

    Console.js

--- Console.js
+++ Console.js
@@ -4,7 +4,7 @@ setTimeout(() => {
  console.log("if you decide to modify the game");
 }, 3000);
 setTimeout(() => {
-  console.log("you can show me! how?");
+  console.log("you can tell me to mod the game the game, and ill do it! how? you might ask.");
 }, 5000);
 setTimeout(() => {
   console.log("well."); 
