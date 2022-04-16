584cf452988b4155431b5f545b9a74e6980174b8
Jackboby <101992698+Jackboby@users.noreply.github.com>
2022-04-16 14:41:29 -0600

    Console.js

---
+++ Console.js
@@ -0,0 +1,14 @@
+//please read.
+console.log("hey");
+setTimeout(() => {
+ console.log("if you decide to modify the game");
+}, 3000);
+setTimeout(() => {
+  console.log("you can show me! how?");
+}, 5000);
+setTimeout(() => {
+  console.log("well."); 
+}, 7000);
+setTimeout(() => {
+  console.log("you can contact me on discord, im poop#6900");  
+}, 9000);
